faf381097a27c50c14e43cbf06b82c5ff6f860cd
Shuya Kinjo <a8109058@gmail.com>
2024-08-23 01:01:44 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>AI TalkBuddy</h1>
+
+      <div className="">
+        <button onClick={() => {}}>aaa</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
